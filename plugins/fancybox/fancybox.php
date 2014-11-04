66f3e9634bbbdfcfb8adce7a4fbb04834831de54
Lars Kliesing <info@lgkonline.de>
2014-11-04 08:39:12 +0100

    added fancybox plugin

---
+++ plugins/fancybox/fancybox.php
@@ -0,0 +1,9 @@
+<?php
+        $pluginHeader .= '<script src="plugins/fancybox/lib/jquery.mousewheel-3.0.6.pack.js"></script>';
+        $pluginHeader .= '<script src="plugins/fancybox/source/jquery.fancybox.js"></script>';
+        $pluginHeader .= '<script src="plugins/fancybox/source/jquery.fancybox.pack.js"></script>';
+        $pluginHeader .= '<script>$(document).ready(function() { $(\'.fancybox\').fancybox(); });</script>';
+        $pluginHeader .= '<link rel="stylesheet" type="text/css" href="plugins/fancybox/source/jquery.fancybox.css" />';
+        
+        $pluginFooter .= '<a href="http://fancyapps.com/fancybox" target="_blank">fancyBox plugin by fancyApps</a>';
+?>
